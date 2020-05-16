9111ba41ad903a2bf6694c3a15c37ec68a3dccec
andy208 <1029123033@qq.com>
2020-05-16 21:24:37 +0800

    02-登录功能实现

---
+++ admin/js/login.js
@@ -0,0 +1,32 @@
+$(function () {
+    //单击登录按钮，发送数据到服务器，可以给form表单注册提交事件，但是submit按钮有默认提交行为，所以要先阻止默认行为
+    $('.login_form').on('submit', function (e) {
+        e.preventDefault()
+        $.ajax({
+            type: 'post',
+            url: 'http://localhost:8080/api/v1/admin/user/login',
+            data: $('.login_form').serialize(),
+            beforeSend: function () {
+                var flag = true
+                $('.login_form input[name]').each(function (index, ele) {
+                    if ($.trim($(ele).val()) == '') {
+                        flag = false
+                    }
+                })
+                if (!flag) {
+                    $('.modal').modal('show')
+                    $('.modal-body p').text('用户名或密码不能为空')
+                }
+            },
+            success: function (res) {
+                // console.log(res)
+                $('.modal').modal('show')
+                $('.modal-body p').text(res.msg)
+                if (res.code == 200) {
+                    window.location.href = './index.html'
+                }
+            }
+        })
+    })
+
+})
